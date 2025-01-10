923b88326857292eaadefd15b9bb371f429449eb
CarsonPenn <144937704+CarsonPenn@users.noreply.github.com>
2025-01-10 13:52:11 -0700

    adding main

--- project1/helloworld.py
+++ project1/helloworld.py
@@ -1,4 +1,8 @@
 # Initial file announcing this repository to the world
 
-print("Hello World, here's to making a difference!")
-print("This is my ActiveLearning repository where I am posting code/projects i am working on to broaden my coding horizons.")
+def main():
+    print("Hello World, here's to making a difference!")
+    print("This is my ActiveLearning repository where I am posting code/projects I am working on to broaden my coding horizons.")
+
+if __name__ == "__main__":
+    main()
