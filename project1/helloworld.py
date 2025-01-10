728740a5ccfc9b07584ed45b7569e1e7ace7f573
CarsonPenn <144937704+CarsonPenn@users.noreply.github.com>
2025-01-10 13:49:59 -0700

    adding project1 folder and helloworld

---
+++ project1/helloworld.py
@@ -0,0 +1,4 @@
+# Initial file announcing this repository to the world
+
+print("Hello World, here's to making a difference!")
+print("This is my ActiveLearning repository where I am posting code/projects i am working on to broaden my coding horizons.")
